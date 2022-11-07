dad147a7dc0a277850af64e0bd759f4ea93ed1b2
Mellef <mellef_med_ali@hotmail.fr>
2022-11-07 22:48:21 +0100

    adding home about

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import './App.css';
 import Decoration from './components/Decoration';
 import Home from './components/Home';
 import Industrie from './components/Industrie';
-import { NavBar } from './components/NavBar';
 import { Sport } from './components/Sport';
-import { Routes , Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Navbar />
       <Routes>
-        <Route path='/'  element={<Home/>} />
-        <Route path='/sport'  element={<Sport/>} />
-        <Route path='/industrie'  element={<Industrie/>} />
-        <Route path='/decoration'  element={<Decoration/>} />
-        <Route path='/contact'  element={<Contact/>} />
-        <Route path='/*'  element={<div>404... Not Found</div>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/sport' element={<Sport />} />
+        <Route path='/industrie' element={<Industrie />} />
+        <Route path='/decoration' element={<Decoration />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/*' element={<div>404... Not Found</div>} />
       </Routes>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,56 @@
-import React from 'react'
-import NavBarExample from './NavBarExample'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from 'react-router-dom'
+import logo from "../data/logo50x60.jpg"
 
-export const NavBar = () => {
+function NavbarC() {
     return (
-        <div>
-            <NavBarExample />
-        </div>
-    )
+        <>
+            <Navbar bg="light" expand="md" className="mb-3">
+                <Container fluid>
+                    <Link className='btn' to="/">
+                        
+                        <img src={logo} alt="logo50x60.jpg"/>
+                        <span className='tw'>TWEMMAN</span>
+                    </Link>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+                    <Navbar.Offcanvas
+                        id={`offcanvasNavbar-expand-md`}
+                        aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+                                Offcanvas
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <Link to="/sport" className='btn'>Sport</Link>
+                                <Link to="/industrie" className='btn'>Industrie</Link>
+                                <Link to="/decoration" className='btn'>Décoration</Link>
+                                <Link to="/contact" className='btn'>Contact</Link>
+                            </Nav>
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+
+        </>
+    );
 }
+
+export default NavbarC;
