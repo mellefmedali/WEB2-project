c52e15171dfb57cad32ac35dd5a5c4b81c62f0b6
mellefmedali <mellef_med_ali@hotmail.fr>
2022-11-15 16:09:43 +0100

    img indus

--- src/components/Industrie.js
+++ src/components/Industrie.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import '../App.css'
 
 const Industrie = () => {
   return (
     <div>
-      <div classNameName="sectIndustrie">
-        <h1><span className="tw">TWEMMAN</span>.INDUSTRIE</h1>
-        <p>TWEMMAN est aussi une entreprise concevant des diverses structures en métal.
+      <div className="sectIndustrie">
+        <img src='./images/industrie/Diapositive1.jpg' style={{width:"auto", height:'270px'}}/>
+        <h1 ><span className='tw'>TWEMMAN</span>.INDUSTRIE</h1>
+        <p><span className='tw'>TWEMMAN</span> est aussi une entreprise concevant des diverses structures en métal.
           Nous réalisons des travaux de métallerie depuis des années ce qui a fait de notre équipe des experts
           ouvrages en fer.
         </p>
         <p>
-          TWEMMAN est en mesure de répondre aux demandes et besoins en constructions et structures
+        <span className='tw'>TWEMMAN</span> est en mesure de répondre aux demandes et besoins en constructions et structures
           métalliques pour tous types d'articles.
         </p>
         <p> Notre expertise permet de répondre à des besoins spécifiques et complexes pour les clients issus de
