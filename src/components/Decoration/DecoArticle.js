2991acc99b2a269687158a9767cad3165f3b727b
mellefmedali <mellef_med_ali@hotmail.fr>
2022-11-17 13:05:14 +0100

    css deco article

--- src/components/Decoration/DecoArticle.js
+++ src/components/Decoration/DecoArticle.js
@@ -6,9 +6,9 @@ function DecoArticle({ list }) {
     return (
         <Card className='decoArticle d-flex flex-row align-items-center'>
             <Card.Img variant="top" src={list.source} className='decoImage' />
-            <Card.Body className='d-flex flex-column w-100'>
+            <Card.Body className='d-flex flex-column w-100 pt-2 pb-2' style={{height:"200px"}}>
                 <Card.Title className="mb-auto text-center pb-2">{list.title}</Card.Title>
-                <Card.Text className="mb-auto text-start">{list.description}</Card.Text>
+                <Card.Text className="mb-auto text-start overflow-auto">{list.description}</Card.Text>
             </Card.Body>
             <Card.Footer className='cardFooter' >
                 <h3 className="font-italic text-nowrap cardFooter">{list.prix} DT</h3>
